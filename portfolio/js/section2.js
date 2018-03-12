dae3504f1041894246b2c0a04c129579c8e30805
geog4046instructor <geog4046instructor@gmail.com>
2018-03-12 15:34:20 -0500

    Update

--- portfolio/js/section2.js
+++ portfolio/js/section2.js
@@ -1,15 +1,32 @@
-// assign values to a set of variables representing a project's metadata
-let projectId = 1
-let projectUrl = 'http://www.arcgis.com/apps/View/index.html?appid=765b50d197f9486c910f13671d8eda5d'
-let projectTitle = 'Map of Recent Earthquakes'
-let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 2.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
-let projectThumb = true
-let projectKeywords = ['USGS', 'earthquakes', 'arcgis online']
+// initialize an array to hold objects that contain project metadata
+let projects = []
 
-// display messages in the browser's developer console, combining the values with text
-console.log('ID: ' + projectId)
-console.log('URL: ' + projectUrl)
-console.log('Title: ' + projectTitle)
-console.log('Description: ' + projectDesc)
-console.log('Has thumbnail? ' + projectThumb)
-console.log('Map extent: ' + projectKeywords)
+// create an object to hold a project's metadata and assign the object to the first position in the projectData array
+projects[0] = {
+  id: 1,
+  url: 'http://bit.ly/2EF070t',
+  title: 'Redlands Tour',
+  desc: 'An ArcGIS Online Story Map Tour showing points of interest in Redlands, CA, creating following the tutorial in Chapter 1 of Getting to Know Web GIS.',
+  thumb: true,
+  keywords: ['Redlands', 'California', 'story map', 'arcgis online']
+}
+
+// create an object to hold a project's metadata and assign the object to the second position in the projectData array
+projects[1] = {
+  id: 2,
+  url: 'http://arcg.is/08WvnO',
+  title: 'Map of Recent Earthquakes',
+  desc: 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.',
+  thumb: false,
+  keywords: ['USGS', 'earthquakes', 'arcgis online']
+}
+
+// step through each element in an array using a "for" loop
+for (let i = 0; i < projects.length; i++) {
+  console.log('Index position ' + i + ' title: ' + projects[i].title) // log the current project's title
+  if (projects[i].thumb === true) {
+    console.log('images/ss-project-' + projects[i].id + '.png') // build and log an image file name based on the project ID
+  } else {
+    console.log('images/no-preview.png')
+  }
+}
