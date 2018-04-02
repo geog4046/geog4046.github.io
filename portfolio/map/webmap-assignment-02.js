7b7685d57e44945f2cbab35b97a59bc7ddb700f9
geog4046instructor <geog4046instructor@gmail.com>
2018-04-02 14:57:00 -0500

    Update

--- portfolio/map/webmap-assignment-02.js
+++ portfolio/map/webmap-assignment-02.js
@@ -1,10 +1,13 @@
-// create a map object
-let mymap = L.map('mapid').setView([37, -95], 4)
+function map2 () {
+  // create a map object
+  let mymap = L.map('map2').setView([37, -95], 4)
 
-// create basemap layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap)
-L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer', {
-  layers: '1',
-  format: 'image/png',
-  transparent: true
-}).addTo(mymap)
+  // create basemap layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap)
+  L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer', {
+    layers: '1',
+    format: 'image/png',
+    transparent: true
+  }).addTo(mymap)
+}
+map2()
