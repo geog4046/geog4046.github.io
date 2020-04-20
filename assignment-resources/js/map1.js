f8f84c5e637234ab8750b3df95fdeda7638b61e2
geog4046instructor <geog4046instructor@gmail.com>
2020-04-20 14:01:39 -0500

    Update

--- assignment-resources/js/map1.js
+++ assignment-resources/js/map1.js
@@ -1,3 +1,5 @@
+/* global L */
+
 function init () {
   var map1 = L.map('map1').setView([32.18, -99.14], 4)
   L.tileLayer('https://tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png?apikey=9e58ec91b076457dbf9835a1932e032c', {
